a577f7d0a24042b796725d28cfe80a6b23b5a37d
Pipe-Runner <piperunner.in@gmail.com>
2023-10-06 07:14:00 +0200

    Updated server url

--- constants/locations.ts
+++ constants/locations.ts
@@ -1,3 +1,3 @@
 export const IMAGE_STORAGE_LOCATION = "PlastOPol";
-export const BASE_URL = "http://192.168.0.121:3000";
+export const BASE_URL = "http://129.241.152.251:3000";
 // "http://129.241.152.251:3000"
